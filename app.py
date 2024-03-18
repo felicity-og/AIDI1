0457cdd3f1081556cec21c0a5a3088d54d86ce4b
felicity-og <felicity.oguntolu@dcmail.ca>
2024-03-18 11:56:19 -0400

    modify app.py

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
-from flask import *
-import sklearn
-from sklearn import preprocessing
+from flask import Flask, render_template, request, url_for
+import numpy as np
 import pickle
 
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb')) 
+pickle_in = open('model.pkl', 'rb')
+model = pickle.load(pickle_in) 
 
 @app.route('/')
 def inputs():
     return render_template('inputs.html')
 
-@app.route('/predictions', methods=['POST'])
+@app.route('/predictions', methods=['GET','POST'])
 def predictions():
     if request.method =='POST':
         result= request.form
-        #result= list(result.values())
+        result= list(result.values())
+        result= np.array(result)
         output = model.predict(result)[0]
-        return render_template("predictions.html",result=output)
+        return render_template("predictions.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
