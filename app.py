8284f4a78af4845a830183b585065a0157da2369
felicity-og <felicity.oguntolu@dcmail.ca>
2024-03-18 12:34:30 -0400

    modify app.py

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, request, url_for
 import pickle
+import os
+import sys
+import warnings
 
+
+
+def likely_error():
+    accept_deprecated_sklearn_package_install = os.environ.get(
+        "SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL", "unset"
+    )
+    accept_deprecated_sklearn_package_install = (
+        accept_deprecated_sklearn_package_install.lower()
+    )
+
+    if accept_deprecated_sklearn_package_install == "true":
+        return
 
 app = Flask(__name__)
 pickle_in = open('model.pkl', 'rb')
 model = pickle.load(pickle_in) 
 
-@app.route('/')
+@app.route('/inputs')
 def inputs():
     return render_template('inputs.html')
 
