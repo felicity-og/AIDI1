d2713f32ce8eccbc1ea576e89e864213c0450c58
felicity-og <felicity.oguntolu@dcmail.ca>
2024-03-18 02:47:15 -0400

    Init app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import *
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb')) 
+
+@app.route('/')
+def inputs():
+    return render_template('inputs.html')
+
+@app.route('/predictions', methods=['POST', 'GET'])
+def predictions():
+    if request.method =='POST':
+        result= request.form.to_dict()
+        result= list(result.values())
+        output = model.predict(result)
+        return render_template("predictions.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
