ea7d0eeb19a61f450529d6b192f58ce1bc62fe91
felicity-og <felicity.oguntolu@dcmail.ca>
2024-03-18 15:29:22 -0400

    modify app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, url_for
 import joblib
-
+import numpy as np
 app = Flask(__name__)
 classifier= joblib.load('fish_classifier.joblib')
 
@@ -11,10 +11,11 @@ def inputs():
 @app.route('/predictions', methods=['GET','POST'])
 def predictions():
     if request.method =='POST':
-        result= request.form
-        result= list(result.values())
-        output = classifier.predict(result)[0]
-        return render_template("predictions.html")
+        measurements = [x for x in request.form.values()]
+        value = [np.array(measurements)]
+        final = np.array(value, dtype=float) 
+        predict = classifier.predict(final)
+        return render_template("predictions.html",predict=predict[0] )
 
 if __name__ == "__main__":
     app.run(debug=True)
